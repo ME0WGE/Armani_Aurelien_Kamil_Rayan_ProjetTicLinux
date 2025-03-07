b2bbd609d61f91273703331efaf239e427f2a857
Aurélien Mertens <gardien243@gmail.com>
2025-03-07 20:29:44 +0100

    aficher le fichier

--- todo.sh
+++ todo.sh
@@ -2,10 +2,21 @@
 
 liste_tache="liste_tache.txt"
 
+
 add_task(){
-    echo "Veuillez entrer une tâche:"
-     read tache
-     echo $tache >> $liste_tache
+  echo "Veuillez entrer une tâche:"
+  read tache
+  echo $tache >> $liste_tache
+}
+
+del_task(){
+  u=1
+  mapfile -t liste < liste_tache.txt
+  for element in "${liste[@]}"
+  do
+    echo "$u. $element"
+    ((u++))
+  done
 }
 
 while true
@@ -16,21 +27,21 @@ do
 3. Afficher toutes les tâches
 4. Quitter
 > " cmd
-    if [ $cmd == "1" ]
+  if [ $cmd == "1" ]
     then
-        clear
-        add_task
+      clear
+      add_task
     elif [ $cmd == "2" ]
     then
-        clear
-        echo "2"
+      clear
+      del_task
     elif [ $cmd == "3" ]
     then
-    clear
-        cat $liste_tache
+      clear
+      cat $liste_tache
     elif [ $cmd == "4" ]
     then
-        break
-        clear
-    fi
+      break
+      clear
+  fi
 done

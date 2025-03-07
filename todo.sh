69aefcf096c4bb01f2e03a4a902022482f94d670
Aurélien Mertens <gardien243@gmail.com>
2025-03-07 21:54:24 +0100

    je crois que ça fonctionne un peu

--- todo.sh
+++ todo.sh
@@ -6,17 +6,31 @@ liste_tache="liste_tache.txt"
 add_task(){
   echo "Veuillez entrer une tâche:"
   read tache
-  echo $tache >> $liste_tache
+  echo $tache >> liste_tache.txt
 }
 
 del_task(){
-  u=1
+  liste_suppresion=()
+  nouvel_list=()
+  u=0
   mapfile -t liste < liste_tache.txt
   for element in "${liste[@]}"
   do
     echo "$u. $element"
     ((u++))
   done
+  read -p "supprimer quel element (un nombre ) :
+  > " nbs
+  u=0
+  for element in "${liste[@]}"
+  do
+    if [[ $u -ne $nbs ]]
+    then
+      nouvel_list+=("$element")
+    fi
+    ((u++))
+  done
+  echo "${nouvel_list[@]}" > liste_tache.txt
 }
 
 while true
@@ -28,20 +42,20 @@ do
 4. Quitter
 > " cmd
   if [ $cmd == "1" ]
-    then
-      clear
-      add_task
-    elif [ $cmd == "2" ]
-    then
-      clear
-      del_task
-    elif [ $cmd == "3" ]
-    then
-      clear
-      cat $liste_tache
-    elif [ $cmd == "4" ]
-    then
-      break
-      clear
+  then
+    clear
+    add_task
+  elif [ $cmd == "2" ]
+  then
+    clear
+    del_task
+  elif [ $cmd == "3" ]
+  then
+    clear
+    cat liste_tache.txt
+  elif [ $cmd == "4" ]
+  then
+    break
+    clear
   fi
 done

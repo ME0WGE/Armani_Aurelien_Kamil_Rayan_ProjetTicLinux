1a7ac17173fe8f4ff4d641e49bb18ee9f0469dea
Kamil <baldygakamil@gmail.com>
2025-03-09 20:55:59 +0100

    fix

--- todo.sh
+++ todo.sh
@@ -15,48 +15,6 @@ message_bienvenue() {
     echo "║     Bienvenue dans votre ToDo APP       ║"
     echo "╚═════════════════════════════════════════╝"
 }
-add_Modif() {
-    # Vérifier si le fichier existe et contient des tâches
-    if [[ ! -f $Liste_tache || ! -s $Liste_tache ]]; then
-        echo "Aucune tâche enregistrée."
-        return
-    fi
-
-    # Afficher la liste des tâches
-    echo "Liste des tâches :"
-    cat "$Liste_tache"
-
-    # Demander l'index de la tâche à modifier
-    echo "Veuillez entrer l'index de la tâche à modifier :"
-    read index
-
-    # Vérifier si l'index est valide
-    if ! [[ $index =~ ^[0-9]+$ ]]; then
-        echo "Erreur : L'index doit être un nombre."
-        return
-    fi
-
-    # Extraire la tâche actuelle
-    old_task=$(sed -n "${index}p" "$Liste_tache")
-
-    # Vérifier si la tâche existe
-    if [[ -z "$old_task" ]]; then
-        echo "Erreur : Aucune tâche trouvée à l'index $index."
-        return
-    fi
-
-    # Afficher la tâche actuelle
-    echo "Tâche actuelle : $old_task"
-    
-    # Demander la nouvelle tâche
-    echo "Vous souhaitez modifier $old_task par:"
-    read new_task
-
-    # Modifier la tâche dans le fichier
-    sed -i "${index}s/.*/$index. $new_task/" "$Liste_tache"
-
-    echo "Tâche $index modifiée avec succès ! La tâche est désormais $new_task !"
-}
 # Afficher une tâche
 afficher_tache() {
     echo "|=== Afficher les Tâches ===|"
@@ -179,7 +137,7 @@ do
             ;;
         3)
             clear
-            modifier_tache
+            add_modif
             read -p "Appuyez sur ENTER pour continuer..."
             ;;
         4)

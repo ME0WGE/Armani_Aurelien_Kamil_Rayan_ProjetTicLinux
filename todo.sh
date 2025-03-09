78d4eb5b0c74f0085572fd24f0b14f9f22857b26
Kamil <baldygakamil@gmail.com>
2025-03-09 20:08:00 +0100

    test

--- todo.sh
+++ todo.sh
@@ -1,55 +1,35 @@
 #!/bin/bash
 
+liste_tache="liste_tache.txt"
 
-
-
-
-
-
-
-
-# Boucle du programme
 while true
 do
-    message_bienvenue
-    echo "==========================================="
-    echo "| 1. Ajouter une tâche                    |"
-    echo "| 2. Supprimer une tâche                  |"
-    echo "| 3. Modifier une tâche                   |"
-    echo "| 4. Afficher toutes les tâches           |"
-    echo "| 5. Quitter                              |"
-    echo "==========================================="
-    read -p "Choisissez une option (1-5)> " choix
-    
-    case $choix in
-        1)
-            clear
-            ajouter_tache
-            read -p "Appuyez sur ENTER pour continuer..."
-            ;;
-        2)
-            clear
-            supprimer_tache
-            read -p "Appuyez sur ENTER pour continuer..."
-            ;;
-        3)
-            clear
-            modifier_tache
-            read -p "Appuyez sur ENTER pour continuer..."
-            ;;
-        4)
-            clear
-            afficher_tache
-            read -p "Appuyez sur ENTER pour continuer..."
-            ;;
-        5)
-            clear
-            echo "Bye :>"
-            exit 0
-            ;;
-        *)
-            echo "Option invalide. Veuillez entrer un nombre entre 1 et 4."
-            read -p "Appuyez sur ENTER pour continuer..."
-            ;;
-    esac
+    read -p "Entrez une commande: 
+1. Ajouter une tâche
+2. Supprimer une tâche
+3. Afficher toutes les tâches
+4. Rechercher une tâche
+5. Quitter
+> " cmd
+    if [ $cmd == "1" ]
+    then
+        clear
+        echo "1"
+    elif [ $cmd == "2" ]
+    then
+        clear
+        echo "2"
+    elif [ $cmd == "3" ]
+    then
+        clear
+        echo "3"
+    elif [ $cmd == "4" ]
+    then
+        clear
+        echo "4"
+    elif [ $cmd == "5" ]
+    then
+        break
+        clear
+    fi
 done
